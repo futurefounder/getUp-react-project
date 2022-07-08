a3759746da43eb2efb7cac474674ed9b4f6dbe2e
f21 <jsk@posteo.de>
2022-07-08 13:49:00 +0200

    add hours and minutes state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import "./styles.css";
 // What do I have to think about in order to get the value from QuantityPicker to Countdown?
-// OK You’ll want to look at using onChange to get the value from the picker, 
+// ✅ You’ll want to look at using onChange to get the value from the picker, 
 // then look storing it int the state of the app component.
-// https://reactjs.org/tutorial/tutorial.html#lifting-state-up or If using hooks  
+// ✅ https://reactjs.org/tutorial/tutorial.html#lifting-state-up or If using hooks https://reactjs.org/docs/hooks-state.html  
 // What do I have to think about when I want the countdown component not to be visible, but rather work “in the background”?
 // Option 1 is to return null in the react-countdown renderer while it’s counting down
 // Option 2 is to create your own countdown using setTimeout or setInterval in the app component. Probably easier to go with option 1 :)
@@ -15,30 +15,34 @@ import Countdown from 'react-countdown';
 
 function App() {  
   
-  const [count, setCount] = useState(0);
-  
+  const [countHours, setCountHours] = useState(0);
+  const [countMinutes, setCountMinutes] = useState(0);
+
   return (
       <div>
           <div className="App" >
             Hours <QuantityPicker 
                     max={24} 
                     min={0} 
-                    onChange={(value) => setCount(value = count)}
+                    onChange={(value) => setCountHours(value)}
                     smooth/>
+                    {/* <p>Hours Selected {countHours}</p> */}
                     {/* Test Button to test state */}
-                    <button onClick={() => setCount(count + 1)}>
+                    {/* <button onClick={() => setCount(count + 1)}>
                     Click me
                     </button>
-                    <p>You clicked {count} times</p>
+                    <p>You clicked {count} times</p> */}
                     {/* / Test Button to test state */}  
             Minutes <QuantityPicker 
                     value={30} 
                     max={60} 
                     min={0} 
-                    onChange={(value)=>{ console.log(value); }}
+                    onChange={(value) => setCountMinutes(value)}
                     smooth/>
+                    {/* <p>Minutes Selected {countMinutes}</p> */}
+                    {console.log("Hrs:" + countHours,"Mins:" + countMinutes)}
           </div>
-          <button onClick={Completionist()}>Start</button>
+          <button>Start</button>
           <p>Workout</p>  
           <Countdown date={Date.now() + 5000}>
             <Completionist />
